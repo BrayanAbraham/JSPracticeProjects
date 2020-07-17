c671cc8b912451f0c715512bff46c9d3e6c253b3
BrayanAbraham <brayanabraham@gmail.com>
2020-07-17 12:14:32 +0530

    Fetch and Display Repos

--- API/Github Finder/app.js
+++ API/Github Finder/app.js
@@ -13,6 +13,7 @@ searchUser.addEventListener("keyup", (e) => {
           ui.alert("User Not Found", "alert alert-danger");
         } else {
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
       .catch((err) => console.log(err));
--- API/Github Finder/github.js
+++ API/Github Finder/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.CLIENT_ID = "6a136b816447ef7799a5";
     this.CLIENT_SECRET = "4f92a246c9efb270e330240ee76384eff70c4e33";
+    this.repos_count = 5;
+    this.respos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.respos_sort}&client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`
+    );
+
     const profileData = await profileResponse.json();
+    const reposData = await repoResponse.json();
 
     return {
       profile: profileData,
+      repos: reposData,
     };
   }
 }
